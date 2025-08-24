cc8a39acfe4a06ff4271a30fc4372503f73ffe9c
shefat2002 <shefat2002@gmail.com>
2025-08-24 11:47:15 +0600

    Initial Migration ran

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<TaskDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
